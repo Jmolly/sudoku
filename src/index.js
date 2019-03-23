60ea74b1ce96fde0b0917c2790cc162e30a26fa0
jmolly <roshkovskypetr@gmail.com>
2019-03-23 15:55:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+  let workMatrix = matrix;
+
+  for (let i = 0; i < 9; i++)
+    for (let j = 0; j < 9; j++) {
+      if (workMatrix[i][j] === 0) {
+        for (let number = 1; number < 10; number++) {
+          if (isValid(workMatrix, i, j, number)) {
+            workMatrix[i][j] = number;
+            if (solveSudoku(workMatrix))
+              return solveSudoku(workMatrix);
+          }
+          workMatrix[i][j] = 0;
+        }
+        return false;
+      }
+    }
+  return workMatrix;
+}
+
+function isValid(workMatrix, i, j, num) {
+
+  let boxRow = Math.floor(i / 3) * 3;
+  let boxCol = Math.floor(j / 3) * 3;
+  for (let n = 0; n < 9; n++) {
+    if (num === workMatrix[n][j] || num === workMatrix[i][n] 
+      || num === workMatrix[boxRow + n % 3][boxCol + Math.floor(n / 3)]) 
+    return false;
+  }
+
+  return true;
 }
